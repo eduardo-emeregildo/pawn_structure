baf9124d0367ec33f0d743528412f18173f69a5f
Eduardo Emeregildo <emeregildoeduardo@gmail.com>
2024-08-18 12:34:16 -0400

    moved around pawn structure images for build

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ const Card = ({ name, description, img }) => {
   return (
     <div className="card max-[950px]:w-72 w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={`/src/assets/${img}.gif`} alt={`${name} pawn structure`} />
+        <img src={`/${img}.gif`} alt={`${name} pawn structure`} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
